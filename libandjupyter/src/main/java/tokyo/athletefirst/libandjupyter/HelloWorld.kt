76dedabbbf1deb79462dac13fc9cf33d68327e70
AthleteFirst Tokyo <athletefirst.tokyo@example.com>
2025-09-20 06:57:09 +0900

    add class, HelloWorld

---
+++ libandjupyter/src/main/java/tokyo/athletefirst/libandjupyter/HelloWorld.kt
@@ -0,0 +1,9 @@
+package tokyo.athletefirst.libandjupyter
+
+class HelloWorld {
+    companion object {
+        fun formatHelloWorld(name: String): String {
+            return "Ok, $name!"
+        }
+    }
+}
